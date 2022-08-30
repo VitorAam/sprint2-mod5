4af46ffb098f1bca1ed1d7dc50abcb6d632e05ac
Vitor <v.augusto.am@gmail.com>
2022-08-29 23:58:42 -0300

    fix: corrigindo rota de clientes

--- src/pages/Clientes/Clientes.jsx
+++ src/pages/Clientes/Clientes.jsx
@@ -1,7 +1,25 @@
-const Clientes = () => {
-    return (
-        <h1>Clientes vão aparecer aqui</h1>
-    )
-}
+import { useContext } from "react"
+import { Link } from 'react-router-dom'
+import { Contexto } from "../../context/Contexto.jsx"
 
+import './Clientes.css'
+
+const Clientes = () => {
+  const { usuarios } = useContext(Contexto)
+
+    return ( 
+    <div className="clientes-container">
+      <h1>Lista de Clientes Cadastrados</h1>
+      <div>
+      {usuarios.map((cliente, index) => {
+        return (
+          <Link key={index} to={`/cliente/${index + 1}`} ><p>{`Cadastro ${index + 1}: ${cliente.nome} ${cliente.sobrenome}`}</p></Link>
+        )
+      })}
+      </div>
+      
+    </div>
+   )
+}
+ 
 export default Clientes;
